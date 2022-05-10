a3ad7d45ddd64851f93602ef0bbd5274cf4e4124
ladykakki <greta_calamari@hotmail.it>
2022-05-10 16:08:09 +0200

    scaffolding + style+ header

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <AppHeader/>
+  </div>
+</template>
+
+<script>
+    
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader,
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import './style/general.scss';
+
+</style>
