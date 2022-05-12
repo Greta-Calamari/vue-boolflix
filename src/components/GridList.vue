0dab4dc2ba7ed43b30d42a823dbf52b14d663032
ladykakki <greta_calamari@hotmail.it>
2022-05-12 10:56:41 +0200

    mouse over show item

--- src/components/GridList.vue
+++ src/components/GridList.vue
@@ -1,45 +1,45 @@
 <template>
 <section class="main-s">
     <h2 class="title">{{title}}</h2>
-    <div class="row w-75 justify-content-center py-5 m-auto">
-         <div class="col-12 col-sm-4 col-md-3 col-lg-2 p-0" v-for="(item) in items" :key="item.id">
-              <div class="col text-center d-flex flex-column align-items-center justify-content-between">
-                <div>
-                   <img class="w-100 p-1" :src="'https://image.tmdb.org/t/p/original/'+ item.poster_path" alt="">
+    <div class="row w-75 justify-content-center py-5 m-auto" >
+         <div class="col-12 col-sm-4 col-md-3 col-lg-2 p-0" v-for="(item) in items" :key="item.id" >
+            <div class=" rela col text-center d-flex flex-column align-items-center justify-content-between" @mouseover="showItem">
+                
+                <img class="w-100 p-1" :src="'https://image.tmdb.org/t/p/w342/'+ item.poster_path" alt="">
+                <div class="showCard" :class="show ? 'active'  : ''">
                    <h4 class="text-center fw-bold p-2 text-uppercase">{{item.title ? item.title : item.name}}</h4>   
-                </div>
-                <div>
                    <p class="px-2 m-0">{{item.original_title ? item.original_title :item.original_name}}</p>
                    <p class="px-2">{{item.release_date}}</p>
-                   <div>
                    <div v-if="item.original_language === 'en' || item.original_language === 'it' ">
                         <img class="flag-img" :src="require('../assets/images/' + item.original_language + '.jpg')" alt="">
                    </div>
                    <div v-else>
                        <img class="flag-img" :src="require('../assets/images/' + 'world.jpg')" alt="">
                    </div>
-
-
-                   <!-- <p class="px-2">{{item.original_language}}</p>-->
-                   </div>
-            
-
                    <p class="px-2 star">
-                   <i v-for="index in changeNumber(item.vote_average)"
-                                :key="index"
-                                font-awesome-icon
-                                class="fa-solid fa-star">
-                   </i>
+                   <i v-for="index in halfNumber(item.vote_average)" :key="index" class="fa-regular fa-star"></i>
                     </p>  
-                   <!-- <i class="fa-brands fa-adn"></i> -->
+
+            </div>
+        </div>
+        
+    </div>
+</div>
+<!-- :class="show ? 'active' : '' " -->
+                   
                 
                    
 
-                </div>
-              </div>
-        
-         </div>
-     </div>
+                
+                
+
+
+                   
+                   
+            
+
+                   
+                
 
 
 </section>
@@ -58,15 +58,19 @@ export default {
    },
    data(){
        return{
+           show: false,
            
 
        }
    },
    methods:{
        
-        changeNumber(number){
+        halfNumber(number){
             return Math.round(number/2)
         },
+        showItem(){
+         return this.show = true 
+    },
 
    },
    computed:{
@@ -78,6 +82,10 @@ export default {
 <style lang="scss">
 @import "../styles/general.scss";
 
+.rela{
+    position: relative;
+}
+
 .star{
     color: yellow;
 }
@@ -85,10 +93,7 @@ export default {
 .title{
     color: $tt-color;
 }
-// .col{
-//     box-shadow: 5px 10px 20px #0000006b;
-    
-// }
+
 
 .main-s{
     
@@ -113,6 +118,27 @@ p{
     color: rgb(255, 255, 255);
     font-size: 20px;
 }
+
+.active{
+  display: block;
+}
+
+.showCard{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 300px;
+    display: none;
+    padding: 20px 10px;
+    overflow-y: auto;
+}
+    
+    
+
+
+
+
 
 </style>
 
